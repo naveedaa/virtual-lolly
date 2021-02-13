4ba97e565019f1caf5c1180f049e8fa679b8d646
Naveeda <naveeda.hanif.02@gmail.com>
2021-02-13 19:58:56 +0500

    Changes

---
+++ gatsby-node.js
@@ -0,0 +1,84 @@
+const path = require(`path`)
+var faunadb = require("faunadb"),
+  q = faunadb.query
+
+var client = new faunadb.Client({
+  secret: "fnAD-9S3PmACAa8u1WwUBLdRIX1_MiWgicvDXIy7",
+})
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage, createRedirect } = actions
+  const lollyTemplate = path.resolve(`src/templates/lolly.tsx`)
+
+  createRedirect({
+    fromPath: "/lolly/*",
+    toPath: "/.netlify/functions/showLolly?id=:splat",
+    isPermanent: false
+  })
+
+  try {
+    const result = await client.query(
+      q.Map(
+        q.Paginate(q.Match(q.Index("all_lollies"))),
+        q.Lambda(x => q.Get(x))
+      )
+    )
+    console.log(result)
+    return result.data.map(d =>
+      createPage({
+        path: `lolly/${d.data.link}`,
+        component: lollyTemplate,
+        context: {
+          link: d.data.link,
+        },
+      })
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+
+
+
+
+
+
+// const path = require("path")
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+
+//   const { data } = await graphql(`
+//        {
+//         get_v_Card {
+//             getVCard { 
+//               c1
+//                c2
+//                c3
+//                rec
+//                msg
+//                sender
+//                url
+//             }
+//           }
+//         }
+//   `)
+
+//   data.get_v_Card.getVCard.forEach(node => {
+//     createPage({
+//       path: `lolly/${node.url}`,
+//       component: path.resolve("./src/templates/template.js"),
+//       context: {
+//         id: node.url,
+//         c1: node.c1,
+//         c2: node.c2,
+//         c3: node.c3,
+//         url: node.url,
+//         msg: node.msg,
+//         sender: node.sender,
+//         rec: node.rec
+//       },
+//     })
+//   })
+// }
